64c047b440224e8e2188916aa34cf91ad2c96b95
emailaiswarya06 <emailaiswarya06@gmail.com>
2021-11-14 17:59:39 +0530

    Alphabet char check

--- src/upperCaseLowerCase.java
+++ src/upperCaseLowerCase.java
@@ -1,3 +1,7 @@
+/*  Java Assignment Day 1
+
+Assignment :  Program to find the entered character is upper case or lower case*/
+
 import java.util.Scanner;
 
 class charCheck{
@@ -27,9 +31,18 @@ public class upperCaseLowerCase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the character to check upper case or lower case");
-        char charValue = sc.next().charAt(0);
-        charCheck obj = new charCheck(charValue);
-        obj.charCheck();
+        //char charValue = sc.next().charAt(0);
+        String str = sc.next();
+        // A check to make the user to enter only single character
+        if(str.length() ==1) {
+            char charValue = str.charAt(0);
+            charCheck obj = new charCheck(charValue);
+            obj.charCheck();
+        }
+        else{
+            System.out.println("Please enter single character only");
+        }
+
 
     }
 }
